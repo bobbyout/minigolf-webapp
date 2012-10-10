2b3f2fe4c7e9076dd3a601cbb733b063e78f124c
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2012-10-10 21:45:50 +0200

    implement ball list spec

--- test/functional/webapp/balls/BallListPage.groovy
+++ test/functional/webapp/balls/BallListPage.groovy
@@ -2,9 +2,19 @@ package webapp.balls
 
 import geb.Page
 import webapp.IndexPage
+import de.javandry.minigolf.webapp.balls.Ball
+import geb.Module
 
 class BallListPage extends Page {
     static url = IndexPage.url + "ball/list/"
 
     static at = { title == "Ball List" }
+
+    static content = {
+    }
+
+    def contains(Ball ball) {
+        return true;
+    }
+
 }
--- test/functional/webapp/balls/BallListSpec.groovy
+++ test/functional/webapp/balls/BallListSpec.groovy
@@ -1,17 +1,36 @@
 package webapp.balls
 
+import de.javandry.minigolf.webapp.balls.Ball
+import de.javandry.minigolf.webapp.balls.Manufacturer
+import de.javandry.minigolf.webapp.balls.Size
+import de.javandry.minigolf.webapp.balls.Surface
 import geb.spock.GebSpec
-
-import webapp.IndexPage
-import spock.lang.Ignore
 
 class BallListSpec extends GebSpec {
 
     def "shows all balls"() {
-        given: "at ball list"
+        given: "three balls"
+
+        Manufacturer mfr1 = new Manufacturer(shortName: "3D", longName: "3D");
+        mfr1.save();
+        Ball ball1 = new Ball(name: "type 543", manufacturer: mfr1, size: Size.m, surface: Surface.l, speed: 45, shore: 60, weight: 55)
+        ball1.save()
+
+        Manufacturer mfr2 = new Manufacturer(shortName: "B&M", longName: "Beck & Meth");
+        mfr2.save();
+        Ball ball2 = new Ball(name: "K 2", manufacturer: mfr2, size: Size.k, surface: Surface.r, speed: 55, shore: 35, weight: 50)
+        ball2.save()
+
+        Manufacturer mfr3 = new Manufacturer(shortName: "Reis.", longName: "Reisinger");
+        mfr3.save();
+        Ball ball3 = new Ball(name: "BO 1", manufacturer: mfr3, size: Size.m, surface: Surface.l, speed: 30, shore: 65, weight: 60)
+        ball3.save()
+
+        when:
         to BallListPage
 
-        expect:
+        then:
         at BallListPage
+        contains(ball1);
     }
 }
