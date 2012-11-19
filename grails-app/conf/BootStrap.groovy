b465a98c8301636270b126a845203b0cfcc9f681
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2012-11-19 21:04:02 +0100

    + BootStrap manufacturers

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
+import de.javandry.minigolf.webapp.balls.Manufacturer
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!Manufacturer.count) {
+            new Manufacturer(shortName: "3D", longName: "3D").save(failOnError: true)
+            new Manufacturer(shortName: "B&M", longName: "Beck & Meth").save(failOnError: true)
+        }
     }
+
     def destroy = {
+        // nothing to do here
     }
 }
