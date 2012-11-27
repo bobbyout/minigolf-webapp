2b17c66da746170a79bf212cddf58b6a52850298
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2012-11-27 21:40:58 +0100

    % debug grails error on travis-ci

--- test/functional/specs/IndexSpec.groovy
+++ test/functional/specs/IndexSpec.groovy
@@ -1,6 +1,5 @@
 package specs
 
-import org.apache.log4j.Logger
 import pages.BallListPage
 import pages.IndexPage
 
@@ -9,9 +8,9 @@ class IndexSpec extends LoggedInAsUserSpec {
     def "open index page forwards to ball list"() {
         given: "go to index page"
         to IndexPage
-        Logger.getLogger(IndexSpec.class).error(browser.driver.pageSource)
 
         expect: "forwarded to ball list page"
+        browser.driver.pageSource.contains("TEST")
         waitFor { at BallListPage }
     }
 }
