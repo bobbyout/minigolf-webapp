63fb35196e9c3abc4209542d81c3b00ada064c25
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2012-12-27 20:35:24 +0100

    % clean up

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,8 @@ class UrlMappings {
             }
         }
 
-        "/"(view: "/index")
+        "/"(controller: 'ball', action: 'list')
+
         "500"(view: '/error')
         "/status"(view: '/status')
     }
