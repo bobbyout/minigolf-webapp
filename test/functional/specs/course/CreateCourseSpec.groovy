900705f6718857a20f7a9b14db2dfd18f640c9fd
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2013-06-26 22:16:49 +0200

    create course without address spec

--- test/functional/specs/course/CreateCourseSpec.groovy
+++ test/functional/specs/course/CreateCourseSpec.groovy
@@ -8,40 +8,80 @@ import specs.LoggedInAsUserSpec
 
 class CreateCourseSpec extends LoggedInAsUserSpec {
 
+    public static final String aCourseName = "course name"
+
+    def setup() {
+        Course.findByName(aCourseName)?.delete(flush: true)
+    }
+
     def "create course with all properties"() {
         given:
         to CourseListPage
-        report "01"
+        report "go to course list page"
 
         when:
         createButton.click()
-        report "02"
+        report "click create button"
 
         then:
         at CourseCreatePage
 
         when:
-        name.value("course name")
+        name.value(aCourseName)
         type.value("Type 1 - Minigolf")
         address.value("Main Street 123, 45678 Downtown")
-        report "03"
+        report "enter course data"
 
         createButton.click()
-        report "04"
+        report "click create button"
 
         then:
         at CourseShowPage
-        name == "course name"
+        name == aCourseName
         type == "Type 1 - Minigolf"
         address == "Main Street 123, 45678 Downtown"
 
         when:
         listButton.click()
-        report "05"
+        report "click list button"
 
         then:
         at CourseListPage
-        shows(new Course(name: "course name", type: Course.Type.ABT_1, address: "Main Street 123, 45678 Downtown"))
+        shows(new Course(name: aCourseName, type: Course.Type.ABT_1, address: "Main Street 123, 45678 Downtown"))
     }
 
+    def "create course without address"() {
+        given:
+        to CourseListPage
+        report "go to course list page"
+
+        when:
+        createButton.click()
+        report "click create button"
+
+        then:
+        at CourseCreatePage
+
+        when:
+        name.value(aCourseName)
+        type.value("Type 1 - Minigolf")
+        report "enter course data"
+
+        createButton.click()
+        report "click create button"
+
+        then:
+        at CourseShowPage
+        name == aCourseName
+        type == "Type 1 - Minigolf"
+        address == ""
+
+        when:
+        listButton.click()
+        report "click list button"
+
+        then:
+        at CourseListPage
+        shows(new Course(name: aCourseName, type: Course.Type.ABT_1))
+    }
 }
